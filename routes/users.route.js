662e85cbf1766a89f4c691644ed808408d74fc16
kiennguyen131 <sayohyeah17@gmai.com>
2020-05-02 17:03:30 +0700

    done user-delete

--- routes/users.route.js
+++ routes/users.route.js
@@ -14,6 +14,15 @@ router.get('/add-user', function( req, res){
 	res.render('add-user')
 })
 
+router.get('/:id/delete', function (req, res) {
+  var id = req.params.id;
+  db.get('users').remove({ id: id }).write();
+  res.redirect('/users');
+})
+
+
+
+
 router.post('/add-user', function(req, res){
 	req.body.id = shortid.generate();
  	db.get('users').push(req.body).write();
