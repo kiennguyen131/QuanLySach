2c6d2347d61e3724fe712334449aaa8e01278e42
kien <sayohyeah17@gmail.com>
2020-05-03 17:10:35 +0700

    add controller book

--- routes/books.route.js
+++ routes/books.route.js
@@ -1,51 +1,16 @@
 const express = require("express");
-const shortid = require("shortid");
-
-const db = require("../db.js");
+var controller = require("../controllers/books.controller");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("books/index", {
-    books: db.get("books").value()
-  });
-});
+router.get("/", controller.index);
 
-router.get("/:id/delete", (req, res) => {
-  let id = req.params.id;
+router.get("/:id/delete", controller.delete);
 
-  db.get("books")
-    .remove({ id: id })
-    .write();
+router.get("/:id/update", controller.update);
 
-  res.redirect("back");
-});
+router.post("/update", controller.postUpdate);
 
-router.get("/:id/update", (req, res) => {
-  let id = req.params.id;
-  res.render("books/update-title", {
-    id: id
-  });
-});
-
-router.post("/update", (req, res) => {
-  db.get("books")
-    .find({ id: req.body.id })
-    .assign({ title: req.body.title })
-    .write();
-
-  res.redirect("/books");
-});
-
-router.post("/", (req, res) => {
-  req.body.id = shortid.generate();
-
-  db.get("books")
-    .push(req.body)
-    .write();
-  res.redirect("back");
-});
-
-
+router.post("/", controller.postIndex);
 
 module.exports = router;
