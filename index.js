2155941d3e4e758221ac9c76b1f25a3470f2c3cb
kiennguyen131 <sayohyeah17@gmai.com>
2020-05-06 17:04:26 +0700

    add login

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 
 const booksRoute = require("./routes/books.route.js");
 const usersRoute = require("./routes/users.route.js");
 const transactionsRoute = require("./routes/transactions.route.js");
+
+const authMiddlewar = require("./middlewares/auth.midderware");
+
+const authRoute = require("./routes/auth.route.js");
 
 
 const app = express();
@@ -15,14 +20,17 @@ app.set("views", "./views");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use("/books", booksRoute);
-app.use("/users", usersRoute);
-app.use("/transactions", transactionsRoute);
+app.use("/books", authMiddlewar.requireAuth, booksRoute);
+app.use("/users", authMiddlewar.requireAuth, usersRoute);
+app.use("/transactions", authMiddlewar.requireAuth, transactionsRoute);
+app.use("/auth", authRoute);
+
 
 app.listen(4000, () => console.log(`Example app listening at http://localhost:4000`))
 
