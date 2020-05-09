97ee5e5a5472976b1da604fd54738f01cb447a13
kiennguyen131 <sayohyeah17@gmai.com>
2020-05-09 20:55:24 +0700

    limit entrt wrong passeword

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+require('dotenv').config();
 
 const db = require("../db.js");
 
@@ -25,13 +26,25 @@ module.exports.postLogin = (req, res) => {
 
 	bcrypt.compare(password, user.password, function(err, result) {
 		if(!result){
-			res.render('auth/login', {
-			errors: [
-				'Wrong password.'
-			],
-			values: req.body
-			});
-			return;
+			if(process.env.TEMP_WRONG_LOGIN < 3){
+				process.env.TEMP_WRONG_LOGIN++;
+				res.render('auth/login', {
+				errors: [
+					'Wrong password.'
+				],
+				values: req.body
+				});
+				return;
+			}
+			else {
+				process.env.TEMP_WRONG_LOGIN = 0;
+				res.render('auth/login', {
+				errors: [
+					'You have entered too many wrong password, please check your email to change the password.'
+				],
+				});
+				return;
+			}
 		}
 		else {
 			res.cookie('userId', user.id, {
